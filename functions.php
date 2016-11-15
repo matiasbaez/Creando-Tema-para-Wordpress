67dff710f5276697bc1ee5202afc172c3a8fb467
Matias Baez <matiasbaez2512@gmail.com>
2016-11-14 23:30:17 -0300

    Error en cargar scripts solucionado

--- functions.php
+++ functions.php
@@ -13,17 +13,17 @@
 	function recursos_del_tema() {
 		wp_enqueue_style( 'style', get_stylesheet_uri());
 
-		wp_enqueue_script( 'dropotron', get_template_directory_uri().'assets/js/jquery.dropotron.min.js', array('jquery'), '1.4.3', true );
+		wp_enqueue_script( 'dropotron', get_template_directory_uri().'/assets/js/jquery.dropotron.min.js', array('jquery'), '1.4.3', true );
 
-		wp_enqueue_script( 'skel', get_template_directory_uri().'assets/js/skel.min.js', array('jquery'), '3.0.1 ', true );
+		wp_enqueue_script( 'skel', get_template_directory_uri().'/assets/js/skel.min.js', array('jquery'), '3.0.1 ', true );
 	
-		wp_enqueue_script( 'skel-viewport', get_template_directory_uri().'assets/js/skel-viewport.min.js', array('jquery'), '3.0.1 ', true );
+		wp_enqueue_script( 'skel-viewport', get_template_directory_uri().'/assets/js/skel-viewport.min.js', array('jquery'), '3.0.1 ', true );
 
-		wp_enqueue_script( 'util', get_template_directory_uri().'assets/js/util.js', array('jquery'), '1.11.3', true );
+		wp_enqueue_script( 'util', get_template_directory_uri().'/assets/js/util.js', array('jquery'), '1.11.3', true );
 
-		wp_enqueue_script( 'respond', get_template_directory_uri().'assets/js/ie/respond.min.js', array('jquery'), '1.11.3', true );
+		wp_enqueue_script( 'respond', get_template_directory_uri().'/assets/js/ie/respond.min.js', array('jquery'), '1.11.3', true );
 	
-		wp_enqueue_script( 'main', get_template_directory_uri().'assets/js/main.js', array('jquery'), '1.11.3', true );
+		wp_enqueue_script( 'main', get_template_directory_uri().'/assets/js/main.js', array('jquery'), '1.11.3', true );
 	}
 
 	add_action( 'wp_enqueue_scripts', 'recursos_del_tema' );
