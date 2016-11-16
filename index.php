d304535f06e3f554eefa9850947d679119f5696b
Matias Baez <matiasbaez2512@gmail.com>
2016-11-15 22:34:12 -0300

    Archivo para las entradas agregado y contenido estatico del index modificado

--- index.php
+++ index.php
@@ -29,14 +29,14 @@
 
 												?>
 
-											<?php 
-												if ( have_posts() ) :
-													while ( have_posts() ) : the_post(); ?>
+											<?php if ( have_posts() ) : while ( have_posts() ) : the_post(); ?>
 														
 														<div class="4u 12u(mobile)">
 															<section class="box">
 
-																<!-- Agrego la ubicacion de cada imagen -->
+																<!-- Agrego la ubicacion de la imagen del articulo
+																	 de momento con el bloginfo hasta que se active
+																	 la funcion de las imagenes -->
 																<a href="<?php the_permalink(); ?>" class="image featured"><img src="<?php bloginfo('template_directory') ?>/images/pic02.jpg" alt="" /></a>
 																<header>
 																	<h3><?php the_title(); ?></h3>
@@ -55,8 +55,8 @@
 													<h3>No se encontro nada para mostrar</h3>
 
 												<?php endif; ?>
-											?>
-
+											
+											<?php wp_reset_query(); // Resetea este query para que no haya problemas cuando se quiera usar otro ?>
 
 									</section>
 
@@ -71,42 +71,52 @@
 											<h2>The Blog</h2>
 										</header>
 										<div class="row">
-											<div class="6u 12u(mobile)">
-												<section class="box">
+											
+											<?php  
+												// Comienzo a reemplazar el contenido estatico de las entradas
 
-													<!-- Agrego la ubicacion de cada imagen -->
-													<a href="#" class="image featured"><img src="<?php bloginfo('template_directory') ?>/images/pic08.jpg" alt="" /></a>
-													<header>
-														<h3>Magna tempus consequat lorem</h3>
-														<p>Posted 45 minutes ago</p>
-													</header>
-													<p>Lorem ipsum dolor sit amet sit veroeros sed et blandit consequat sed veroeros lorem et blandit  adipiscing feugiat phasellus tempus hendrerit, tortor vitae mattis tempor, sapien sem feugiat sapien, id suscipit magna felis nec elit. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos lorem ipsum dolor sit amet.</p>
-													<footer>
-														<ul class="actions">
-															<li><a href="#" class="button icon fa-file-text">Continue Reading</a></li>
-															<li><a href="#" class="button alt icon fa-comment">33 comments</a></li>
-														</ul>
-													</footer>
-												</section>
-											</div>
-											<div class="6u 12u(mobile)">
-												<section class="box">
+												query_posts(
+													array(
+														'showposts' => 2, // La cantidad a mostrar
+														'cat' => 4		  // La categoria y el id (tambien puede ser por el slug de la categoria)
+													)
+												);
 
-													<!-- Agrego la ubicacion de cada imagen -->
-													<a href="#" class="image featured"><img src="<?php bloginfo('template_directory') ?>/images/pic09.jpg" alt="" /></a>
-													<header>
-														<h3>Aptent veroeros et aliquam</h3>
-														<p>Posted 45 minutes ago</p>
-													</header>
-													<p>Lorem ipsum dolor sit amet sit veroeros sed et blandit consequat sed veroeros lorem et blandit  adipiscing feugiat phasellus tempus hendrerit, tortor vitae mattis tempor, sapien sem feugiat sapien, id suscipit magna felis nec elit. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos lorem ipsum dolor sit amet.</p>
-													<footer>
-														<ul class="actions">
-															<li><a href="#" class="button icon fa-file-text">Continue Reading</a></li>
-															<li><a href="#" class="button alt icon fa-comment">33 comments</a></li>
-														</ul>
-													</footer>
-												</section>
-											</div>
+												?>
+
+											<?php if ( have_posts() ) : while ( have_posts() ) : the_post(); ?>
+
+													<div class="6u 12u(mobile)">
+														<section class="box">
+
+															<!-- Agrego la ubicacion de la imagen, de momento con bloginfo -->
+															<a href="<?php the_permalink(); ?>" class="image featured"><img src="<?php bloginfo('template_directory') ?>/images/pic08.jpg" alt="" /></a>
+															<header>
+																<h3><?php the_title(); ?></h3>
+																<p><?php the_ ?>Posted 45 minutes ago</p>
+															</header>
+															
+															<?php the_content(); // El contenido ?>
+
+															<footer>
+																<ul class="actions">
+																	<li><a href="<?php the_permalink(); ?>" class="button icon fa-file-text">Continue Reading</a></li>
+																	<li><a href="#" class="button alt icon fa-comment">33 comments</a></li>
+																</ul>
+															</footer>
+														</section>
+													</div>
+
+													<?php endwhile; ?>
+													<!-- post navigation -->
+												<?php else: ?>
+													
+													<h3>No se encontro nada para mostrar</h3>
+
+												<?php endif; ?>
+											
+											<?php wp_reset_query(); // Resetea este query para que no haya problemas cuando se quiera usar otro ?>
+
 										</div>
 									</section>
 
--- single.php
+++ single.php
@@ -1,19 +1,74 @@
-<?php 
-	// En este archivo se muestra el contenido de las ENTRADAS
 
-	get_header('page');
+<!-- En este archivo se muestra el contenido de las ENTRADAS -->
 
-	if ( have_posts() ) :
-		while ( have_posts() ) : the_post();
+<?php get_header('page'); ?>
 
-				the_title();
-				the_content();
-			
-		endwhile;
-	else:
-		echo "No se econtro nada para mostrar";
-	endif;
+			<!-- Main -->
+				<div id="main-wrapper">
+					<div class="container">
+						<div class="row">
+							<div class="4u 12u(mobile)">
 
-	get_footer();
+								<!-- Sidebar -->
+									<section class="box">
+										<a href="#" class="image featured"><img src="images/pic09.jpg" alt="" /></a>
+										<header>
+											<h3>Sed etiam lorem nulla</h3>
+										</header>
+										<p>Lorem ipsum dolor sit amet sit veroeros sed amet blandit consequat veroeros lorem blandit  adipiscing et feugiat phasellus tempus dolore ipsum lorem dolore.</p>
+										<footer>
+											<a href="#" class="button alt">Magna sed taciti</a>
+										</footer>
+									</section>
+									<section class="box">
+										<header>
+											<h3>Feugiat consequat</h3>
+										</header>
+										<p>Veroeros sed amet blandit consequat veroeros lorem blandit adipiscing et feugiat sed lorem consequat feugiat lorem dolore.</p>
+										<ul class="divided">
+											<li><a href="#">Sed et blandit consequat sed</a></li>
+											<li><a href="#">Hendrerit tortor vitae sapien dolore</a></li>
+											<li><a href="#">Sapien id suscipit magna sed felis</a></li>
+											<li><a href="#">Aptent taciti sociosqu ad litora</a></li>
+										</ul>
+										<footer>
+											<a href="#" class="button alt">Ipsum consequat</a>
+										</footer>
+									</section>
 
-?>
+							</div>
+
+							<div class="8u 12u(mobile) important(mobile)">
+
+								<!-- Content -->
+								<?php 
+									if ( have_posts() ) :
+										while ( have_posts() ) : the_post(); ?>
+											
+											<article class="box post">
+
+												<!-- De momento se cargan las imagenes con el bloginfo -->
+												<a href="<?php the_permalink(); ?>" class="image featured"><img src="<?php bloginfo('template_url'); ?>/images/pic01.jpg" alt="" /></a>
+												<header>
+													<h2><?php the_title(); ?></h2>
+													<!--p>Lorem ipsum dolor sit amet feugiat</p-->
+												</header>
+												
+												<?php the_content(); // El contenido de la Entrada ?>
+
+											</article>
+
+										<?php endwhile; ?>
+
+									<?php else: ?>
+													
+										<h3>No se encontro nada para mostrar</h3>
+
+									<?php endif; ?>
+
+							</div>
+						</div>
+					</div>
+				</div>
+
+<?php get_footer(); ?>
