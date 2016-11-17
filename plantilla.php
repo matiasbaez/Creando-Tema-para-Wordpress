0a5d100730356d9c84b890750954c2e988bba155
Matias Baez <matiasbaez2512@gmail.com>
2016-11-16 21:23:31 -0300

    Para poder agregar una plantilla personalizada

---
+++ plantilla.php
@@ -0,0 +1,6 @@
+<?php /* Template Name: El_Nombre_de_la_Plantilla */ ?>
+
+<?php 
+	/* La linea de arriba sirve para habilitar la opcion para poder elegir
+	una plantilla con estilos diferentes a la de page.php (EN LAS PAGINAS) */
+?>
